5888ea312999fdabc491835b7fca36e9e8e2037d
yourDoctor <1212121@qq.com>
2017-06-18 13:57:40 +0800

    remoteMasterBase__提交

--- app/src/main/java/com/example/administrator/mycapp/MainActivity.java
+++ app/src/main/java/com/example/administrator/mycapp/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.administrator.mycapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    Toast.makeText(this, "aaa", Toast.LENGTH_LONG);
   }
 }
