bbc26a5a7754539d0ca41ccc624f35ae5c08ec16
yourDoctor <1212121@qq.com>
2017-06-18 15:30:40 +0800

    得得得得

--- app/src/main/java/com/example/administrator/mycapp/MainActivity.java
+++ app/src/main/java/com/example/administrator/mycapp/MainActivity.java
@@ -10,9 +10,6 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    int lengthLong = Toast.LENGTH_LONG;
-    int a = Toast.LENGTH_LONG;
-    int b = Toast.LENGTH_LONG;
 
     Toast.makeText(this, "aa4354353453454355435a", Toast.LENGTH_LONG);
     Toast.makeText(this, "345345435435455345", Toast.LENGTH_LONG);
