a6385561ad248d9e212b6016ee0b459293d88107
yourDoctor <1212121@qq.com>
2017-06-18 13:50:17 +0800

    tjiao

--- app/src/main/java/com/example/administrator/mycapp/MainActivity.java
+++ app/src/main/java/com/example/administrator/mycapp/MainActivity.java
@@ -2,11 +2,17 @@ package com.example.administrator.mycapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    int lengthLong = Toast.LENGTH_LONG;
+    int a = Toast.LENGTH_LONG;
+    int b = Toast.LENGTH_LONG;
+    int e = Toast.LENGTH_LONG;
+    int f = Toast.LENGTH_LONG;
   }
 }
